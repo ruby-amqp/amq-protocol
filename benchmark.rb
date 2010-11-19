555eccf6e2161a45cf874b1a67d4cb84b193eb84
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2010-11-19 15:46:19 +0000

    Added an empty benchmark script.

---
+++ benchmark.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+begin
+  require "amqp/spec"
+rescue LoadError
+  abort "You have to install the amqp gem in order to run the benchmark against its AMQP encoder/decoder."
+end
+
+require "benchmark"
+
+Benchmark.bmbm do |bm|
+  bm.report("AMQP Gem") do
+    # TODO
+  end
+
+  bm.report("AMQP Protocol Gem") do
+    # TODO
+  end
+end
