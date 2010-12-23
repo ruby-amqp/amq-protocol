1e259a31f13573d05890bc6eb352b07a4fabfa01
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2010-12-23 10:01:34 +0000

    Encode channel.

--- examples/00_manual_test.rb
+++ examples/00_manual_test.rb
@@ -9,14 +9,19 @@ socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
 sockaddr = Socket.pack_sockaddr_in((ARGV.first || 5672).to_i, "127.0.0.1") # NOTE: this doesn't work with "localhost", I don't know why.
 
 # helpers
-def socket.encode(klass, *args)
+def socket.encode(channel, klass, *args)
   STDERR.puts "#{klass}.encode(#{args.inspect[1..-2]})"
   result = klass.encode(*args)
   if result.is_a?(Frame)
+    result.channel = channel
     self.write(result.encode)
   else
-    data = result.inject("") { |buffer, frame| buffer += frame.encode }
-    self.write(data)
+    data = result.inject("") do |buffer, frame|
+      frame.channel = channel
+      self.write(frame.encode) ###
+      buffer += frame.encode
+    end
+    # self.write(data)
   end
 end
 
@@ -42,35 +47,37 @@ begin
 
   # Connection.Start/Connection.Start-Ok
   connection_start_response = socket.decode
-  socket.encode Connection::StartOk, {client: "AMQ Protocol"}, "PLAIN", "guest\0guest\0", "en_GB"
+  socket.encode 0, Connection::StartOk, {client: "AMQ Protocol"}, "PLAIN", "guest\0guest\0", "en_GB"
 
   # Connection.Tune/Connection.Tune-Ok
   connection_tune_response = socket.decode
   channel_max = connection_tune_response.channel_max
   frame_max   = connection_tune_response.frame_max
   heartbeat   = connection_tune_response.heartbeat
-  socket.encode Connection::TuneOk, channel_max, frame_max, heartbeat
+  socket.encode 0, Connection::TuneOk, channel_max, frame_max, heartbeat
   puts "Max agreed frame size: #{frame_max}"
 
   # Connection.Open/Connection.Open-Ok
-  socket.encode Connection::Open, "/"
+  socket.encode 0, Connection::Open, "/"
   connection_open_ok_response = socket.decode
 
   # Channel.Open/Channel.Open-Ok
-  puts "========================"
-  socket.encode Channel::Open, ""
+  socket.encode 1, Channel::Open, ""
   channel_open_ok_response = socket.decode
 
   # Exchange.Declare/Exchange.Declare-Ok
-  socket.encode Exchange::Declare, "tasks", "fanout", false, false, false, false, {}
+  socket.encode 1, Exchange::Declare, "tasks", "fanout", false, false, false, false, {}
   exchange_declare_ok_response = socket.decode
 
+  # Basic.Publish
+  socket.encode 1, Basic::Publish, "this is a payload", {a: 1}, "tasks", "", false, false, frame_max
+
   # Channel.Close/Channel.Close-Ok
-  socket.encode Channel::Close
+  socket.encode 1, Channel::Close, 200, "bye", 0, 0
   channel_close_ok_response = socket.decode
 
   # Connection.Close/Connection.Close-Ok
-  socket.encode Connection::Close, 200, "", 0, 0
+  socket.encode 0, Connection::Close, 200, "", 0, 0
   # are these good args?
   # reply_code, reply_text, class_id, method_id = 200, "", 0, 0
   close_ok_response = socket.decode
