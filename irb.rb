f20872b66b41d9ecc1125e921bc3a529fefcd364
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2010-12-23 12:50:19 +0000

    Improved irb.rb, it will load ~/.irbrc first, then it will run our code. There might seem there's no obvious reason, but some people (like me) might want to set some variables in their irbrc based on already loaded files and then it screws things up if the loading order is reverted.
    For more discussion see https://gist.github.com/752908

--- irb.rb
+++ irb.rb
@@ -1,9 +1,44 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
+# This file is supposed to make inspecting AMQ protocol easier.
+
+# How does it work:
+# 1) This file is executed.
+# 2) We load irb, redefine where IRB looks for .irbrc and start IRB.
+# 3) IRB loads .irbrc, which we redefined, so it loads this file again.
+#    However now the second branch of "if __FILE__ == $0" gets executed,
+#    so it runs our custom code which loads the original .irbrc and then
+#    it redefines some IRB settings. In this case it add IRB hook which
+#    is executed after IRB is started.
+
+# Although it looks unnecessarily complicated, I can't see any easier
+# solution to this problem in case that you need to patch original settings.
+# Obviously in case you don't have the need, you'll be happy with simple:
+
+# require "irb"
+#
+# require_relative "lib/amq/protocol/client.rb"
+# include AMQ::Protocol
+#
+# IRB.start(__FILE__)
+
 require "irb"
-require_relative "lib/amq/protocol/client.rb"
 
-include AMQ::Protocol
+if __FILE__ == $0
+  puts "~ Using #{__FILE__} as an executable ..."
 
-IRB.start(__FILE__)
+  def IRB.rc_file_generators
+    yield Proc.new { |_| __FILE__ }
+  end
+
+  IRB.start(__FILE__)
+else
+  irbrc = File.join(ENV["HOME"], ".irbrc")
+  puts "~ Using #{__FILE__} as a custom .irbrc .."
+  puts "~ Loading original #{irbrc} ..."
+  load irbrc
+
+  require_relative "lib/amq/protocol/client.rb"
+  include AMQ::Protocol
+end
