df7638d597efbc4f0faf30962e8e6077d109954a
Jakub Šťastný aka Botanicus <stastny@101ideas.cz>
2011-02-09 16:16:13 +0000

    The manual example works again.

--- examples/00_manual_test.rb
+++ examples/00_manual_test.rb
@@ -1,10 +1,16 @@
-# encoding: binary
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# Usage:
+#   ./examples/00_manual_test.rb
+#   ./examples/00_manual_test.rb 5673
 
 require "socket"
 require_relative "../lib/amq/protocol/client.rb"
 
 include AMQ::Protocol
 
+# Stolen from amq-client:
 class AMQ::Protocol::Frame
   def self.decode(io)
     header = io.read(7)
@@ -20,8 +26,8 @@ class AMQ::Protocol::Frame
   end
 end
 
-socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
 # NOTE: this doesn't work with "localhost", I don't know why:
+socket   = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
 sockaddr = Socket.pack_sockaddr_in((ARGV.first || 5672).to_i, "127.0.0.1")
 
 # helpers
@@ -62,7 +68,7 @@ begin
 
   # Connection.Start/Connection.Start-Ok
   connection_start_response = socket.decode
-  socket.encode Connection::StartOk, {client: "AMQ Protocol"}, "PLAIN", "guest\0guest\0", "en_GB"
+  socket.encode Connection::StartOk, {client: "AMQ Protocol"}, "PLAIN", "\0guest\0guest", "en_GB"
 
   # Connection.Tune/Connection.Tune-Ok
   connection_tune_response = socket.decode
@@ -83,21 +89,21 @@ begin
 
     begin
       # Exchange.Declare/Exchange.Declare-Ok
-      socket.encode Exchange::Declare, 1, "tasks", "fanout", false, false, false, false, {}
+      socket.encode Exchange::Declare, 1, "tasks", "fanout", false, false, false, false, false, {}
       exchange_declare_ok_response = socket.decode
 
       # Queue.Declare/Queue.Declare-Ok
-      socket.encode Queue::Declare, 1, "", false, false, false, false, {}
+      socket.encode Queue::Declare, 1, "", false, false, false, false, false, {}
       queue_declare_ok_response = socket.decode
 
       puts "Queue name: #{queue_declare_ok_response.queue.inspect}"
 
       # Queue.Bind/Queue.Bind-Ok
-      socket.encode Queue::Bind, 1, queue_declare_ok_response.queue, "tasks", "", {}
+      socket.encode Queue::Bind, 1, queue_declare_ok_response.queue, "tasks", "", false, {}
       queue_bind_ok_response = socket.decode
 
       # Basic.Consume
-      socket.encode Basic::Consume, 1, queue_declare_ok_response.queue, "", false, false, false, Hash.new
+      socket.encode Basic::Consume, 1, queue_declare_ok_response.queue, "", false, false, false, false, Hash.new
 
       # Basic.Publish
       socket.encode Basic::Publish, 1, "this is a payload", {content_type: "text/plain"}, "tasks", "", false, false, frame_max
@@ -107,7 +113,7 @@ begin
       puts "Consumed successfully, consumer tag: #{basic_consume_ok_response.consumer_tag}"
 
       # Basic.Deliver
-      basic_deliver = socket.decode
+      basic_deliver        = socket.decode
       basic_deliver_header = socket.decode # header frame: {}
       basic_deliver_body   = socket.decode # body frame: "this is a payload"
       puts "[Received] headers: #{basic_deliver_header.inspect}, payload: #{basic_deliver_body.inspect}"
@@ -119,8 +125,6 @@ begin
   ensure
     # Connection.Close/Connection.Close-Ok
     socket.encode Connection::Close, 200, "", 0, 0
-    # are these good args?
-    # reply_code, reply_text, class_id, method_id = 200, "", 0, 0
     close_ok_response = socket.decode
   end
 rescue Exception => exception
@@ -129,7 +133,7 @@ rescue Exception => exception
     line = "\e[0;36m#{line}\e[0m" if line.match(Regexp::quote(File.basename(__FILE__)))
     STDERR.puts "  - " + line
   end
-  exit 1
+  exit 1 # Yes, this works with the ensure block, even though the rescue block run first. Magic.
 ensure
   socket.close
 end
