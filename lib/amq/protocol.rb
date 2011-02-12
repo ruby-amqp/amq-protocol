9b7ed0cc00af4adf75a6da5d17b5cb9298e7c8c1
Michael S. Klishin <michael@novemberain.com>
2011-02-12 23:20:29 +0300

    Traces spam

--- lib/amq/protocol.rb
+++ lib/amq/protocol.rb
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 __dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 
-puts __dir
 $:.unshift(__dir) unless $:.include?(__dir)
 
 require "amq/protocol/client"
