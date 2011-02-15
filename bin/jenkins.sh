8f38a0f39e8ad08072fe04f8c91dd6c1f35dbf34
Jakub Šťastný aka Botanicus <stastny@101ideas.cz>
2011-02-15 18:18:46 +0000

    Use Bash, otherwise RVM will complain, install only local gems and pull the changes manually.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # Server-specific, I have RVM setup at the profile file:
 source /etc/profile
 
-rvm 1.9.2-head,1.8.7 exec bundle install && rspec spec
+git fetch && git reset origin/master --hard
+rvm 1.9.2-head,1.8.7 exec bundle install --local && rspec spec
