6f5cc3aa25ac701a13bb53753ae066564c75bcac
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-03-08 02:55:45 +0000

    Can you please work for f*** sake :/ ?!

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -5,13 +5,17 @@ source /etc/profile
 git fetch && git reset origin/master --hard
 
 echo -e "\n\n==== Ruby 1.9.2 Head ===="
-rvm 1.9.2-head@ruby-amqp exec bundle install --local; echo
-rvm 1.9.2-head@ruby-amqp exec bundle exec rspec spec
+rvm use 1.9.2-head@ruby-amqp
+gem install bundle
+bundle install --local; echo
+bundle exec rspec spec
 return_status=$?
 
 echo -e "\n\n==== Ruby 1.8.7 ===="
-rvm 1.8.7@ruby-amqp exec bundle install --local; echo
-rvm 1.8.7@ruby-amqp exec bundle exec rspec spec
+rvm use 1.8.7@ruby-amqp
+gem install bundle
+bundle install --local; echo
+bundle exec rspec spec
 return_status=$(expr $return_status + $?)
 
 test $return_status -eq 0
