b6d8133e55a65de16c0c44662c0e4ad90874747c
Theo <theo@iconara.net>
2011-03-08 12:48:52 +0100

    Changed from constants using PACK_CACHE

--- lib/amq/protocol/table.rb
+++ lib/amq/protocol/table.rb
@@ -1,5 +1,8 @@
 # encoding: binary
 
+require 'amq/protocol/client'
+
+
 module AMQ
   module Protocol
     class Table
@@ -8,18 +11,8 @@ module AMQ
           super("Invalid table value on key #{key}: #{value.inspect} (#{value.class})")
         end
       end
-      
-      LC_C  = 'c'.freeze
-      LC_Q  = 'q'.freeze
-      UC_CN = 'CN'.freeze
-      UC_D  = 'D'.freeze
-      UC_F  = 'F'.freeze
-      UC_I  = 'I'.freeze
-      UC_N  = 'N'.freeze
-      UC_N2 = 'N2'.freeze
-      UC_S  = 'S'.freeze
-      UC_T  = 'T'.freeze
-      TEN   = '10'.freeze
+
+      TEN = '10'.freeze
 
       def self.encode(table)
         buffer = String.new
@@ -30,36 +23,36 @@ module AMQ
 
           case value
           when String then
-            buffer << UC_S
-            buffer << [value.bytesize].pack(UC_N)
+            buffer << PACK_CACHE[:S]
+            buffer << [value.bytesize].pack(PACK_CACHE[:N])
             buffer << value
           when Integer then
-            buffer << UC_I
-            buffer << [value].pack(UC_N)
+            buffer << PACK_CACHE[:I]
+            buffer << [value].pack(PACK_CACHE[:N])
           when TrueClass, FalseClass then
             value = value ? 1 : 0
-            buffer << UC_I
-            buffer << [value].pack(UC_N)
+            buffer << PACK_CACHE[:I]
+            buffer << [value].pack(PACK_CACHE[:N])
           when Hash then
-            buffer << UC_F # it will work as long as the encoding is ASCII-8BIT
+            buffer << PACK_CACHE[:F] # it will work as long as the encoding is ASCII-8BIT
             buffer << self.encode(value)
           when Time then
             # TODO: encode timezone?
-            buffer << UC_T
-            buffer << [value.to_i].pack(LC_Q).reverse # Don't ask. It works.
+            buffer << PACK_CACHE[:T]
+            buffer << [value.to_i].pack(PACK_CACHE[:q]).reverse # Don't ask. It works.
           else
             # We don't want to require these libraries.
             if defined?(BigDecimal) && value.is_a?(BigDecimal)
-              buffer << UC_D
+              buffer << PACK_CACHE[:D]
               if value.exponent < 0
                 decimals = -value.exponent
                 # p [value.exponent] # normalize
                 raw = (value * (decimals ** 10)).to_i
                 #pieces.append(struct.pack('>cBI', 'D', decimals, raw)) # byte integer
-                buffer << [decimals + 1, raw].pack(UC_CN) # somewhat like floating point
+                buffer << [decimals + 1, raw].pack(PACK_CACHE[:CN]) # somewhat like floating point
               else
                 # per spec, the "decimals" octet is unsigned (!)
-                buffer << [0, value.to_i].pack(UC_CN)
+                buffer << [0, value.to_i].pack(PACK_CACHE[:CN])
               end
             else
               raise InvalidTableError.new(key, value)
@@ -67,43 +60,43 @@ module AMQ
           end
         end
 
-        [buffer.bytesize].pack(UC_N) + buffer
+        [buffer.bytesize].pack(PACK_CACHE[:N]) + buffer
       end
 
       def self.length(data)
-        data.unpack(UC_N).first
+        data.unpack(PACK_CACHE[:N]).first
       end
 
       def self.decode(data)
         table = Hash.new
-        size = data.unpack(UC_N).first
+        size = data.unpack(PACK_CACHE[:N]).first
         offset = 4
         while offset < size
-          key_length = data.slice(offset, 1).unpack(LC_C).first
+          key_length = data.slice(offset, 1).unpack(PACK_CACHE[:c]).first
           offset += 1
           key = data.slice(offset, key_length)
           offset += key_length
           type = data.slice(offset, 1)
           offset += 1
           case type
-          when UC_S then
-            length = data.slice(offset, 4).unpack(UC_N).first
+          when PACK_CACHE[:S] then
+            length = data.slice(offset, 4).unpack(PACK_CACHE[:N]).first
             offset += 4
             value = data.slice(offset, length)
             offset += length
-          when UC_I then
-            value = data.slice(offset, 4).unpack(UC_N).first
+          when PACK_CACHE[:I] then
+            value = data.slice(offset, 4).unpack(PACK_CACHE[:N]).first
             offset += 4
-          when UC_D then
-            decimals, raw = data.slice(offset, 5).unpack(UC_CN)
+          when PACK_CACHE[:D] then
+            decimals, raw = data.slice(offset, 5).unpack(PACK_CACHE[:CN])
             offset += 5
             value = BigDecimal.new(raw.to_s) * (BigDecimal.new(TEN) ** -decimals)
-          when UC_T then
+          when PACK_CACHE[:T] then
             # TODO: what is the first unpacked value??? Zone, maybe? It's 0, so it'd make sense.
-            timestamp = data.slice(offset, 8).unpack(UC_N2).last
+            timestamp = data.slice(offset, 8).unpack(PACK_CACHE[:N2]).last
             value = Time.at(timestamp)
             offset += 8
-          when UC_F then
+          when PACK_CACHE[:F] then
             value = self.decode(data.slice(offset, data.bytesize - offset))
           else
             raise "Not a valid type: #{type.inspect}\nData: #{data.inspect}\nUnprocessed data: #{data[offset..-1].inspect}"
