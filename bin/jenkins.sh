52f0ade79c33494342969dff0d8d5deaa267bc91
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-03-08 02:03:08 +0000

    Use gemsets.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -4,13 +4,15 @@ echo -e "\n\n==== Setup ===="
 git fetch && git reset origin/master --hard
 
 echo -e "\n\n==== Ruby 1.9.2 Head ===="
-rvm 1.9.2-head exec bundle install --local; echo
-rvm 1.9.2-head exec bundle exec rspec spec
+rvm use 1.9.2-head@ruby-amqp
+bundle install --local; echo
+bundle exec rspec spec
 return_status=$?
 
 echo -e "\n\n==== Ruby 1.8.7 ===="
-rvm 1.8.7 exec bundle install --local; echo
-rvm 1.8.7 exec bundle exec rspec spec
+rvm use 1.8.7@ruby-amqp
+bundle install --local; echo
+bundle exec rspec spec
 return_status=$(expr $return_status + $?)
 
 test $return_status -eq 0
