314c048ef2cc0416337bf3b20799f24d07c366fa
Theo <theo@iconara.net>
2011-03-17 09:14:46 +0100

    Reorganized the AMQ::Protocol::Method specs

--- spec/amq/protocol/client_spec.rb
+++ spec/amq/protocol/client_spec.rb
@@ -16,23 +16,23 @@ module AMQ
       end
       
       describe '.encode_body' do
-        it 'encodes a body into a BodyFrame, when it fits in one frame' do
-          body_frames = Method.encode_body('Hello world', 1, 131072)
-          body_frames.first.payload.should == 'Hello world'
-          body_frames.first.channel.should == 1
-          body_frames.should have(1).item
+        context 'when the body fits in a single frame' do
+          it 'encodes a body into a BodyFrame' do
+            body_frames = Method.encode_body('Hello world', 1, 131072)
+            body_frames.first.payload.should == 'Hello world'
+            body_frames.first.channel.should == 1
+            body_frames.should have(1).item
+          end
         end
 
-        it 'encodes a body into a number of BodyFrames, when it is too big to fit' do
-          lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-          body_frames = Method.encode_body(lipsum, 1, 100)
-          body_frames.map(&:payload).should == [
-            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut',
-            ' labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la',
-            'boris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volu',
-            'ptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat no',
-            'n proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-          ]
+        context 'when the body is to big to fit in a single frame' do
+          it 'encodes a body into a list of BodyFrames that each fit within the frame size' do
+            lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            frame_size = 100
+            expected_payload_size = 93
+            body_frames = Method.encode_body(lipsum, 1, frame_size)
+            body_frames.map(&:payload).should == lipsum.split('').each_slice(expected_payload_size).map(&:join)
+          end
         end
       end
     end
