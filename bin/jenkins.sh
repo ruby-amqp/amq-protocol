d38acc65df54cfdf87a8f3002deaf8527e3675fd
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-03-21 11:10:13 +0000

    [CI] Install to vendor/bundle instead to system.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -7,7 +7,7 @@ git fetch && git reset origin/master --hard
 echo -e "\n\n==== Ruby 1.9.2 Head ===="
 rvm use 1.9.2-head@ruby-amqp
 gem install bundler --no-ri --no-rdoc
-bundle install --local; echo
+bundle install --local --path vendor/bundle; echo
 bundle exec rspec spec
 return_status=$?
 
