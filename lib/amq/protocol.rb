72ed0b6d02eebdfa3d8b287ef1566b3bece6bcd4
Michael S. Klishin <michael@novemberain.com>
2011-04-16 07:33:52 +0400

    Add amq/protocol/version, settle on 0.5.0.pre

--- lib/amq/protocol.rb
+++ lib/amq/protocol.rb
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
+require "amq/protocol/version"
 require "amq/protocol/client"
---
+++ lib/amq/protocol/version.rb
@@ -0,0 +1,5 @@
+module AMQ
+  module Protocol
+    VERSION = "0.5.0.pre".freeze
+  end # Protocol
+end # AMQ
