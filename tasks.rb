de9321b367e77e27ea79332e1060de3bc7259548
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-06-08 20:05:21 +0200

    You can optionally specify target for the generate task.
      Example:
        ./tasks.rb generate --targets=client,server

--- tasks.rb
+++ tasks.rb
@@ -1,20 +1,23 @@
-#!/usr/bin/env nake
+#!/usr/bin/env bundle exec nake
 # encoding: utf-8
 
 # load "contributors.nake"
 
+# ./tasks.rb generate
+# ./tasks.rb generate --targets=client,server
 Task.new(:generate) do |task|
   task.description = "Generate lib/amq/protocol/client.rb"
   task.define do |opts, spec = nil|
+    opts[:targets] ||= ["client"]
+
     if spec.nil?
       spec = "vendor/rabbitmq-codegen/amqp-rabbitmq-0.9.1.json"
       unless File.exist?(spec)
         sh "git submodule update --init"
       end
     end
-    
-    targets = %w{client} # add "server" to generate server code
-    targets.each do |type|
+
+    opts[:targets].each do |type|
       path = "lib/amq/protocol/#{type}.rb"
       sh "./codegen.py #{type} #{spec} #{path}"
       if File.file?(path)
